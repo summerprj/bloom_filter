4999cc721bb6cb0282e6c1cf8c2b6b64da278e9c
m <monster911@mail.ru>
2019-06-04 14:11:32 +0300

    in prog

--- test_bloom.c
+++ test_bloom.c
@@ -4,8 +4,9 @@
 #include <string.h>
 #include "bloom.h"
 
-void read_file_into_filter() {
-	char* filename = "in.txt";
+void read_file_into_filter(char* filename) {
+//void read_file_into_filter() {
+	//char* filename = "in.txt";
 	FILE* file = fopen(filename, "r");
 	if (file != NULL) {
 		unsigned char line[128];//буферная строка
@@ -16,10 +17,12 @@ void read_file_into_filter() {
 	}
 }
 
-void check_key_and_write_to_file() {
-	char* filename_keys = "out_keys.txt";
+void check_key_and_write_to_file(char* filename_keys, char* filename_out) {
+	//char* filename_keys = "out_keys.txt"; - это строки не будет
+//void check_key_and_write_to_file() {
+	//char* filename_keys = "out_keys.txt";
 	FILE* file_keys = fopen(filename_keys, "r");
-	char* filename_out = "out.txt";
+	//char* filename_out = "out.txt";
 	FILE* file_out = fopen(filename_out, "w");
 	if (file_keys!= NULL && file_out != NULL) {
 		unsigned char key[128];//буферная строка
@@ -30,12 +33,34 @@ void check_key_and_write_to_file() {
 		fclose(file_keys);
 		fclose(file_out);
 	}
+	else {
+		printf("Can not open file(s)");
+	}
 }
 
-int main() {
-	init_bloom_filter(2000);
+int main(int argc, char* argv[]) {
+	int n;
+	char* filename_in;
+	char* filename_keys;
+	char* filename_out;
+	if (argc == 5) {
+		n = atoi(argv[1]);
+		filename_in = argv[2];
+		filename_keys = argv[3];
+		filename_out = argv[4];
+	} else{
+		printf("Incorrect value\n");
+		exit(-1);
+	}
+	printf("Parameters: k=%d in=%s out-key=%s out=%s\n", n, filename_in, filename_keys, filename_out);
+	init_bloom_filter(n);
+	read_file_into_filter(filename_in);
+	check_key_and_write_to_file(filename_keys,filename_out);
+	
+		/*init_bloom_filter(1);
 	read_file_into_filter();
 	check_key_and_write_to_file();
+	*/
 	return 0;
 }
 
