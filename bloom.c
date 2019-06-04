8e8e673780ad502d841ab758756bf29b08b5b18a
m <monster911@mail.ru>
2019-06-04 06:43:06 +0300

    in progress

--- bloom.c
+++ bloom.c
@@ -10,6 +10,10 @@ int bloom_filter_size;
 void init_bloom_filter(int size) {
 	bloom_filter_size = size;
 	bloom_filter = (unsigned int*)malloc(sizeof(unsigned int) * bloom_filter_size);
+	for (int i = 0; i < bloom_filter_size; i++) {
+		bloom_filter[i] = 0;
+		printf("bloom_fil0 %u\n", bloom_filter[i]);
+	}
 }
 
 //Хэш-функции
@@ -21,7 +25,7 @@ unsigned int hash(unsigned char* str, int k)
 	for (int i = 0; *(str+i) != '\0'; i++) {
 		c = *(str+i);
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-		hash *= k + 1;
+		hash *= (k + 1);
 	}
 	return hash;
 }
@@ -29,17 +33,20 @@ unsigned int hash(unsigned char* str, int k)
 //Кладем в Блум фильтер строки, которые будем сравнивать
 void put(unsigned char* str) {
 	for (int i = 0; i < bloom_filter_size; i++) {
-		long key_i = hash(str, i);
-		bloom_filter[i] = key_i | bloom_filter[i];
+		unsigned int key_i = hash(str, i);
+		bloom_filter[i] = (key_i | bloom_filter[i]);
+		printf("bloom_fil %u\n", bloom_filter[i]);
 	}
-	
+	printf("str in put %s\n", str);
 }
 
 //Проверяем, есть ли эта строка в Фильтре, если хоть раз 1,
 //то дальше не интерируемся по всем фильтрам, а выводим true
 int mightContain(unsigned char* str) {
+	printf("size %d\n", bloom_filter_size);
 	for (int i = 0; i < bloom_filter_size; i++) {
-		long key_i = hash(str, i);
+		unsigned int key_i = hash(str, i);
+		printf("key %u\n", key_i);
 		if ((bloom_filter[i] & key_i) == key_i) {
 			return 1;
 		}
--- test_bloom.c
+++ test_bloom.c
@@ -4,32 +4,28 @@
 #include <string.h>
 #include "bloom.h"
 
-void read_file() {
-	char* token;
-	char* sep = " ";//separator
+void read_file_into_filter() {
 	char* filename = "in.txt";
 	FILE* file = fopen(filename, "r");
 	if (file != NULL) {
 		char line[128];//буферная строка
 		while (fgets(line, sizeof line, file) != NULL) {
-			token = strtok(line, sep);
-			printf("1 %s", token);
-			token = strtok(NULL, sep);
-			printf("2 %s",token);
+			put(line);
+			printf("line %s\n", line);
 		}
 		fclose(file);
 	}
 }
 
 int main(int argc, char* argv[]) {
-	read_file();
-	init_bloom_filter(10);
-	char* key1 = "Hi";
-	char* key2 = "Yep";
-	char* key3 = "Bla";
+	init_bloom_filter(2);
+	read_file_into_filter();
+	char* key1 = "12";
+	char* key2 = "34";
+	char* key3 = "45678542";
 	char* key4 = "Bla-Bla";
-	put(key1);
-	put(key2);
+	//put(key1);
+	//put(key2);
 	printf("Bloom filter should contain key: %s , test result: %s\n",key1, btoa(mightContain(key1)));
 	printf("Bloom filter should contain key: %s , test result: %s\n", key2, btoa(mightContain(key2)));
 	printf("Bloom filter shouldn't contain key: %s , test result: %s\n", key3, btoa(mightContain(key3)));
